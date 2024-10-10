7975d9ebbc942e439d587fafc7d07d424ac8578e
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-10-09 22:30:33 -0300

    refactor: refactoring views to use class-based views

--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -1,43 +1,33 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Livro
-from .serializers import LivroSerializer
+from rest_framework import generics
+from .models import Livro, Categoria, Autor
+from .serializers import LivroSerializer, CategoriaSerializer, AutorSerializer
 
-@api_view(['GET', 'POST'])
-def livro_list_create(request):
-    if request.method == 'GET': 
-        livros = Livro.objects.all()
-        serializer = LivroSerializer(livros, many=True)
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
-        serializer = LivroSerializer(data=request.data)
+class LivroList(generics.ListCreateAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-list"
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-detail"
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def livro_detail(request, pk):
-    livro = Livro.objects.get(pk=pk)
+class CategoriaList(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-list"
 
-    if request.method == 'GET':
-        serializer = LivroSerializer(livro)
-        return Response(serializer.data)
-    
-    if request.method == 'PUT':
-        serializer = LivroSerializer(livro, data=request.data)
+class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-detail"
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    if request.method == 'DELETE':
-        livro.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class AutorList(generics.ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-list"
+
+class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-detail"
