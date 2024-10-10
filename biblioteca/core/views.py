0434b57fc66ac0a5b75d8014729de8825a442b87
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-10-09 21:54:31 -0300

    refactor: excluding the csrf_exempt decorator and adding api_view

--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def livro_list_create(request):
     if request.method == 'GET': 
         livros = Livro.objects.all()
@@ -15,13 +15,13 @@ def livro_list_create(request):
     if request.method == 'POST':
         serializer = LivroSerializer(data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def livro_detail(request, pk):
     livro = Livro.objects.get(pk=pk)
 
